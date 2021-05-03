c728bbb9caeb7aab43a5ddb36139b2fb10e631a3
Paulina Hetman <info@pehaa.com>
2021-05-03 07:32:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Planets from "./components/Planets"
 function App() {
   return (
     <section className="container  p-3">
-      <h1>Planètes dans l'univer Star Wars</h1>
+      <h1>Planètes dans l'univers Star Wars</h1>
       <Planets />
     </section>
   )
