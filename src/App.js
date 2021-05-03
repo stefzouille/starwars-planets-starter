2cedaaef9457645437bcbb868fd1f37260aef4d5
stefzouille <contact.lokcy@gmail.com>
2021-05-03 12:21:52 +0200

    nice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useState } from "react"
 import Planets from "./components/Planets"
 
 function App() {
+  const [active, setActive] = useState(false);
   return (
     <section className="container  p-3">
-      <h1>Planètes dans l'univers Star Wars</h1>
-      <Planets />
+      <h1>Planètes dans l'univer Star Wars</h1>
+      <div className="form-check form-switch mb-3">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          id="activate"
+          onChange={() => setActive((active) => !active)}
+        />
+        <label className="form-check-label" htmlFor="activate">
+          Activer
+        </label>
+      </div>
+      {active && <Planets />}
     </section>
-  )
+  );
 }
 
 export default App
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,18 +1,94 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Planet from "./Planet"
 
 const Planets = () => {
   const [planets, setPlanets] = useState([])
+   const [loading, setLoading] = useState(false)
+   const [error, setError] = useState("")
+   const [page, setPage] = useState(1)
+   const [hasNext, setHasNext] = useState(null);
 
-  return (
+ useEffect(() => {
+   console.log('use effect start with', page)
+    setLoading(true)
+    setError("")
+    let isCancelled = false
+    const controller = new AbortController()
+    //setPage(1)
+    fetch(`https://swapi.dev/api/planets/?page=${page}` ,{
+     signal: controller.signal,
+  })
+      .then((response) => {
+        console.log("don't forget me here!!!");
+        return new Promise((resolved) => {
+          setTimeout(() => resolved(response), 2000);
+        });
+      })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `Nous n'avons pas pu lire les registres des planètes, status : ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('i get data')
+        console.log(data);
+        if (!isCancelled) {
+          console.log("i will update component")
+        setPlanets((p) => [...p, ...data.results]);
+        setHasNext(data.next)
+        setLoading(false);
+        }
+      })
+      .catch((error) => {
+        console.log(error.message)
+        if (!isCancelled) {
+        setError(error.message);
+        setLoading(false);
+        }
+      });
+      return () => {
+        console.log('clean up')
+        console.log("i want to cancel")
+        isCancelled = true
+        controller.abort()
+      }
+  }, [page]);
+
+return (
     <>
       <div className="row">
         {planets.map((planet) => {
-          return <Planet key={planet.name} planet={planet} />
+          return <Planet key={planet.name} planet={planet} />;
         })}
       </div>
+      {loading && <p className="text-center">loading...</p>}
+      {!!error && <p className="alert alert-danger">{error}</p>}
+      {hasNext && (
+        <button
+          type="button"
+          className="btn btn-dark"
+          onClick={() => {
+            setPage(page + 1);
+          }}
+          disabled={loading}
+        >
+          Suivantes
+        </button>
+      )}
+
+      {hasNext === false && (
+        <p className="bg-dark text-white p-3">
+          Nous avons listé toutes les planètes recensées.
+        </p>
+      )}
     </>
-  )
-}
+  );
+};
+
+
+
 
 export default Planets
